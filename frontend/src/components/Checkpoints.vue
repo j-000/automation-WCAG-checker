57171fab765d38f2995590423c27b4e1f224cd1d
j-000 <jjasilva85@gmail.com>
2019-07-17 19:19:05 +0100

    Proof of concept

--- frontend/src/components/Checkpoints.vue
+++ frontend/src/components/Checkpoints.vue
@@ -1,83 +1,88 @@
 <template>
-  <div>
-    <h1>Checkpoints</h1>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>WCAG Levels</th>
-          <th>Benefits</th>
-          <th>Regex</th>
-        </tr> 
-      </thead>
-      <tbody>
-        <tr 
-        v-for="checkpoint in checkpoints"
-        v-bind:key="checkpoint.id"
-        >
-          <td>{{ checkpoint.id }}</td>
-          <td>{{ checkpoint.name }}</td>
-          <td>{{ checkpoint.wcaglevels }}</td>
-          <td>{{ checkpoint.benefits }}</td>
-          <td>{{ checkpoint.regex }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <hr>
-    <div class="addForm">
-      <b-form @submit="addCheckpoint">
-        <b-form-group id="input-group-1" label="Id:" label-for="input-1">
-          <b-form-input
-          id="input-1"
-          v-model="form.id"
-          type="number"
-          required
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-2" label="Checkpoint Name:" label-for="input-2">
-          <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="Checkpoint name"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-3" label="WCAG Levels:" label-for="input-3">
-          <b-form-input
-          id="input-3"
-          v-model="form.wcaglevels"
-          required
-          placeholder="A, AA, AAA"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-4" label="Benefits:" label-for="input-4">
-          <b-form-input
-          id="input-4"
-          v-model="form.benefits"
-          required
-          placeholder="Accessibility, SEO, Usability"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-form-group id="input-group-5" label="Regex:" label-for="input-5" description="Please escape the backslahsh with another backslash.">
-          <b-form-input
-          id="input-5"
-          v-model="form.regex"
-          required
-          placeholder="(?si)\\b"
-          ></b-form-input>
-        </b-form-group>
-
-        <b-button type="submit" variant="primary">Add</b-button>
-      </b-form>
+  <div class="row">
+    <div class="col text-left">
+      <h1>Checkpoints</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>WCAG Levels</th>
+            <th>Benefits</th>
+            <th>Regex</th>
+          </tr> 
+        </thead>
+        <tbody>
+          <tr 
+          v-for="checkpoint in checkpoints"
+          v-bind:key="checkpoint.id"
+          >
+            <td>{{ checkpoint.id }}</td>
+            <td>{{ checkpoint.name }}</td>
+            <td>{{ checkpoint.wcaglevels }}</td>
+            <td>{{ checkpoint.benefits }}</td>
+            <td>{{ checkpoint.regex }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
-    <div class="alert" v-bind:class="{ 'alert-success': success, 'alert-danger': error  }">
-      <p>{{ add_form_message }}</p>
+    <div class="col text-left">
+      <h1>Add a new checkpoint</h1>
+      <div class="addForm">
+        <b-form @submit="addCheckpoint">
+          <b-form-group id="input-group-1" label="Id:" label-for="input-1">
+            <b-form-input
+            id="input-1"
+            v-model="form.id"
+            type="number"
+            required
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-2" label="Checkpoint Name:" label-for="input-2">
+            <b-form-input
+            id="input-2"
+            v-model="form.name"
+            required
+            placeholder="Checkpoint name"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-3" label="WCAG Levels:" label-for="input-3">
+            <b-form-input
+            id="input-3"
+            v-model="form.wcaglevels"
+            required
+            placeholder="A, AA, AAA"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-4" label="Benefits:" label-for="input-4">
+            <b-form-input
+            id="input-4"
+            v-model="form.benefits"
+            required
+            placeholder="Accessibility, SEO, Usability"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-form-group id="input-group-5" label="Regex:" label-for="input-5">
+            <b-form-input
+            id="input-5"
+            v-model="form.regex"
+            required
+            placeholder="Example: (?si)<html[^>]*(comphero)[^>]*>"
+            ></b-form-input>
+          </b-form-group>
+
+          <b-button type="submit" variant="primary">Add</b-button>
+        </b-form>
+      </div>
+      <div class="alert" v-bind:class="{ 'alert-success': success, 'alert-danger': error  }">
+        <p>{{ add_form_message }}</p>
+      </div>
     </div>
+    
   </div>
 </template>
 
--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,35 +1,38 @@
 <template>
-  <div>
-    <div class="alert"  v-bind:class="{ 'alert-success': success, 'alert-danger': error  }">
-      <p>{{ response }}</p>
-    </div>
-    <b-form @submit="onSubmit" v-if="show">
-      <b-form-group
-        id="input-group-1"
-        label="Email address:"
-        label-for="input-1"
-        description="We'll never share your email with anyone else."
-      >
-        <b-form-input
-          id="input-1"
-          v-model="form.email"
-          type="email"
-          required
-          placeholder="Enter email"
-        ></b-form-input>
-      </b-form-group>
+  <div class="row">
+    <div class="col-6 m-auto text-left">
+      <h1>Register</h1>
+      <div class="alert" v-if="response" v-bind:class="{ 'alert-success': success, 'alert-danger': error  }">
+        <p>{{ response }}</p>
+      </div>
+      <b-form @submit="onSubmit" v-if="show">
+        <b-form-group
+          id="input-group-1"
+          label="Email address"
+          label-for="input-1"
+          description="We'll never share your email with anyone else."
+        >
+          <b-form-input
+            id="input-1"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
 
-      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
-        <b-form-input
-          id="input-2"
-          v-model="form.name"
-          required
-          placeholder="Enter name"
-        ></b-form-input>
-      </b-form-group>
+        <b-form-group id="input-group-2" label="Your name" label-for="input-2">
+          <b-form-input
+            id="input-2"
+            v-model="form.name"
+            required
+            placeholder="Enter name"
+          ></b-form-input>
+        </b-form-group>
 
-      <b-button type="submit" variant="primary">Submit</b-button>
-    </b-form>
+        <b-button type="submit" variant="success">Submit</b-button>
+      </b-form>
+    </div>    
   </div>
 </template>
 
@@ -43,7 +46,7 @@
           name: ''
         },
         show: true,
-        response: '',
+        response: false,
         success: false,
         error: false
       }
